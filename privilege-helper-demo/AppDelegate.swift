69cb05576d584e3b74a6bc7e917e9df5ddde5685
alaneuler <alaneuler@qq.com>
2023-01-09 20:13:01 +0800

    Initial Commit

---
+++ privilege-helper-demo/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  privilege-helper-demo
+//
+//  Created by Alaneuler Erving on 2023/1/9.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+
---
+++ privilege-helper-demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  privilege-helper-demo
+//
+//  Created by Alaneuler Erving on 2023/1/9.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
